588f4d065b06e9cdc0a5e1b0641f04f47d546a60
anjali sharma <anjalisharma@anjalis-MacBook-Air.local>
2015-09-22 23:44:17 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+1+1
 }
 
 
@@ -12,4 +12,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  1+1
 }
